ee78f66bbb0c0a35244a797c392a5a3820c72d72
FrancisMich <franciskasiemobi@gmail.com>
2022-12-15 14:03:31 -0800

    hoyfix

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - check if a letter is uppercase
+ * @c: input character
+ * *Return: 1 if uppercase, 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
