35b1a9d28a4271392f8b792bd7371cbe3aeb2dfd
FrancisMich <franciskasiemobi@gmail.com>
2022-12-19 12:49:26 -0800

    to copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy string
+ *
+ * @src: source
+ *
+ * @buffer: destination
+ *
+ * Return: thr pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
+	return (dest);
+}
