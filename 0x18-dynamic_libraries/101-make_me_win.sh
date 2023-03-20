7a69f651d96752ad0d746857023d9fdbcfa9d39a
FrancisMich <franciskasiemobi@gmail.com>
2023-03-20 15:43:44 -0700

    Task4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -P https://github.com/FrancisMich/alx-low_level_programming/blob/master/0x18-dynamic_libraries/hacked.so
+wget -P /tmp https://github.com/FrancisMich/alx-low_level_programming/blob/master/0x18-dynamic_libraries/hacked.so
 export LD_PRELOAD=/tmp/hacked.so
